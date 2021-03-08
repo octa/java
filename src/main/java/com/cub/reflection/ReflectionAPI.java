3990dafac72f8a916344c68e0bb93ed0bff6f03f
khan <khan@kh.octa.com>
2021-03-07 20:33:19 -0600

    Initial commit

---
+++ src/main/java/com/cub/reflection/ReflectionAPI.java
@@ -0,0 +1,14 @@
+package com.cub.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionAPI {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        ModelClass modelClass = new ModelClass(10, "Hi");
+
+    }
+
+}
